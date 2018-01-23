3366c792d860ddb55ff14aa8e3487f5c4e963c80
Rajesh Ramadoss <mr.rajeshr@gmail.com>
2018-01-23 01:02:02 +0000

    fixed config issue which was changed earlier for local execution purpose

--- microservices/ui/app/src/hasuraExamples/config.js
+++ microservices/ui/app/src/hasuraExamples/config.js
@@ -1,9 +1,9 @@
 //var REACT_APP_CLUSTER_NAME = 'beseeching73'
 var projectConfig = {
   url: {
-    data: "https://data." + REACT_APP_CLUSTER_NAME + ".hasura-app.io/v1/query",
-    auth: "https://auth." + REACT_APP_CLUSTER_NAME + ".hasura-app.io/v1",
-    filestore: "https://filestore." + REACT_APP_CLUSTER_NAME + ".hasura-app.io/v1/file"
+    data: "https://data." + process.env.REACT_APP_CLUSTER_NAME + ".hasura-app.io/v1/query",
+    auth: "https://auth." + process.env.REACT_APP_CLUSTER_NAME + ".hasura-app.io/v1",
+    filestore: "https://filestore." + process.env.REACT_APP_CLUSTER_NAME + ".hasura-app.io/v1/file"
   }
 }
 
